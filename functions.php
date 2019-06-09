7de2aa3572647b605a7e82da54dbdd72ae667cb6
Beda Schmid <arbeit@yoonect.com>
2019-06-09 21:03:55 +0700

    Update 1.1 stable
    - Added API WP > JS (localized script)
    - Added callbacks for data feed
    - Removed 2 notices due to undefined vars

--- functions.php
+++ functions.php
@@ -2,6 +2,21 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+function countries_visited(){
+	$countries_visited = get_posts();
+	//Etc
+}
+
+function js_php_api(){
+	$api_array = array(
+	    'siteURL' => site_url(),
+	    'post_id' => get_the_ID(),
+	    'countries_visited' => countries_visited(),
+		'page_slug' => 'country'
+	);
+	return $api_array;
+}
+
 function enqueue_tmg_scripts() {
 
 	$scripts_array_urls = [
@@ -10,12 +25,13 @@ function enqueue_tmg_scripts() {
 		"https://d3js.org/topojson.v2.min.js", 
 		"https://bl.ocks.org/mbostock/raw/7ea1dde508cec6d2d95306f92642bc42/6aac691494f752142a67cc43c51a0fd09896dbd4/versor.js"
 	];
-	
-	foreach ($scripts_array_urls as $scripts_array_url) {
-		$script_srcs .= "<script src=" . $scripts_array_url . "></script>";
-	}
+	$my = implode("></script><script src=", $scripts_array_urls);
+	$outmy = "<script src=" . $my. "></script>";
+	// foreach ($scripts_array_urls as $scripts_array_url) {
+	// 	$script_srcs .= "<script src=" . $scripts_array_url . "></script>";
+	// }
 
-	return $script_srcs;
+	return $outmy;
 
 }
 
@@ -28,14 +44,16 @@ function return_tmg_canvas() {
 
 function load_tmg_customized_script() {
 
-	$script_url = plugin_dir_url( 'D3-Globe-Rendering-for-WordPress.php' ) . '/D3-Globe-Rendering-for-WordPress/maps-globus-custom.js';
-	$script_src = "<script src=" . $script_url . "></script>";
-	return $script_src;
+	wp_register_script( 'maps_custom_globus', plugin_dir_url( 'D3-Globe-Rendering-for-WordPress.php' ) . '/D3-Globe-Rendering-for-WordPress/maps-globus-custom.js' );
+ 
+	// Localize the script with new data
+	wp_localize_script( 'maps_custom_globus', 'php_api_object', js_php_api() );
+	wp_enqueue_script( 'maps_custom_globus', plugin_dir_url( 'D3-Globe-Rendering-for-WordPress.php' ) . '/D3-Globe-Rendering-for-WordPress/maps-globus-custom.js', array(), 1.0, false );
 
 }
 
 function load_all(){
-	$out .= enqueue_tmg_scripts();
+	$out = enqueue_tmg_scripts();
 	$out .= return_tmg_canvas();
 	$out .= load_tmg_customized_script();
 return $out;
