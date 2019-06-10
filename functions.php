8d1730bf39f7bdfc629422e41cb650ae3774ae08
Beda Schmid <arbeit@yoonect.com>
2019-06-10 18:57:46 +0700

    Adding methods for single map rendering
    Single map with several shortcode options

--- functions.php
+++ functions.php
@@ -3,8 +3,24 @@
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 function countries_visited(){
-	$countries_visited = get_posts();
-	//Etc could be a view returning this array ["704", "116", "764", "76", "604", "484", "840"]
+	
+	$countries_visited = array(
+		'posts_per_page'   => -1,
+		'post_type'        => 'country',
+		'post_status'      => 'publish',
+		'suppress_filters' => true,
+		);
+	$posts_array = get_posts( $countries_visited );
+	
+	$country_ids = array();
+
+	foreach ( $posts_array as $country_id ) {
+		$country_ids[] = get_post_meta( $country_id->ID, 'wpcf-country-json-id', true );
+	}
+	
+	$country_ids = '["'.implode('","', $country_ids).'"]';
+	
+	return $country_ids;
 }
 
 function js_php_api(){
@@ -51,9 +67,68 @@ function load_tmg_customized_script() {
 
 }
 
-function load_all(){
+function d3_globe(){
 	$out = enqueue_tmg_scripts();
 	$out .= return_tmg_canvas();
 	$out .= load_tmg_customized_script();
 return $out;
+}
+
+function load_single_country_svg($country){
+	
+	$country_svg_file_name = match_country_code_svg($country);
+
+	$file = file_get_contents(plugin_dir_url( 'D3-Globe-Rendering-for-WordPress.php' ) . '/D3-Globe-Rendering-for-WordPress/country-svg-maps/' . $country_svg_file_name . '.svg');
+
+	return $file;
+}
+
+function apply_static_styles($areas, $css) {
+	$areas = explode(',',$areas);
+	foreach ($areas as $area) {
+		$styles[] = '#' . $area . '{' . $css . ';}';
+	}
+	$all_area_styles = '<style>' . implode('</style><style>', $styles) . '</style>';
+	return $all_area_styles;
+}
+
+function apply_hover_styles($areas, $css) {
+	$areas = explode(',', $areas);
+	foreach ($areas as $area) {
+		$styles[] = '#' . $area . ':hover{' . $css . ';}';
+	}
+	$all_area__hover_styles = '<style>' . implode('</style><style>', $styles) . '</style>';
+	return $all_area__hover_styles;
+}
+
+function match_country_code_svg($country){
+	$decoded = json_decode( file_get_contents(plugin_dir_url( 'D3-Globe-Rendering-for-WordPress.php' ) . '/D3-Globe-Rendering-for-WordPress/country-codes.json', true));
+	foreach ($decoded as $value) {
+		if ($value->id == $country) {
+			$country = str_replace(' ', '-', strtolower($value->name));
+		}
+	}
+
+	return $country;
+}
+
+function svg_single_country( $atts ) {
+	$atts = shortcode_atts( array(
+		'country' => '',
+		'country_css' => '',
+		'borders_css' => '',
+		'active_areas' => '',//we need comma separated values like 1,2,3
+		'active_areas_css' => '',
+		'active_areas_hover_css' => '',
+		'active_areas_click_script' => '',
+		'active_areas_hover_script' => '',
+		'active_areas_link' => '',
+		'width' => '',
+		'height' => ''
+	), $atts, 'bartag' );
+
+	$svg = load_single_country_svg($atts['country']);
+	$svg .= apply_static_styles($atts['active_areas'], $atts['active_areas_css']);
+	$svg .= apply_hover_styles($atts['active_areas'], $atts['active_areas_hover_css']);
+	return $svg;
 }
